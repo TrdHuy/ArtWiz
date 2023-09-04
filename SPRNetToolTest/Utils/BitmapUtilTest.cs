11333346c0488169c6d1fd4883b3b08ef66b29c7
Tran Huy <trdtranduchuy@gmail.com>
2023-09-04 21:11:20 +0700

    Thêm BitmapUtilTest

---
+++ SPRNetToolTest/Utils/BitmapUtilTest.cs
@@ -0,0 +1,42 @@
+using SPRNetTool.Utils;
+using System.Windows.Media;
+
+namespace SPRNetToolTest.Utils
+{
+    public class BitmapUtilTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestLoadBitmapSourceFromFile()
+        {
+            string imagePath = "Resources\\test.png".FullPath();
+            var bmpSource = BitmapUtil.LoadBitmapFromFile(imagePath);
+            Assert.NotNull(bmpSource);
+            Assert.That(bmpSource.PixelWidth * bmpSource.PixelHeight, Is.EqualTo(90000));
+        }
+
+        [Test]
+        public async Task TestCountColors()
+        {
+            string imagePath = "Resources\\test.png".FullPath();
+            var bmpSource = BitmapUtil.LoadBitmapFromFile(imagePath);
+            Assert.NotNull(bmpSource);
+            var src = await BitmapUtil.CountColorsAsync(bmpSource);
+            Assert.That(src.Count, Is.EqualTo(4));
+
+            var redColor = Color.FromArgb(255, 237, 28, 36);
+            var yellowColor = Color.FromArgb(255, 255, 242, 0);
+            var greenColor = Color.FromArgb(255, 34, 177, 76);
+            var blueColor = Color.FromArgb(255, 0, 162, 232);
+
+            Assert.That(src[redColor], Is.EqualTo(21456));
+            Assert.That(src[yellowColor], Is.EqualTo(23999));
+            Assert.That(src[greenColor], Is.EqualTo(22499));
+            Assert.That(src[blueColor], Is.EqualTo(22046));
+        }
+    }
+}
