aa73c196514826825a12538a58c2a8af62f88029
TrdHuy <32306489+TrdHuy@users.noreply.github.com>
2023-09-20 18:39:34 +0700

    Update sam_run.bat

--- sam_run.bat
+++ sam_run.bat
@@ -1,5 +1,6 @@
 @echo off
 
+set scriptName=%~nx0
 set current_dir=%CD%
 set sam_path=%~1
 set src_path=%~2
@@ -8,20 +9,62 @@ set cleanCMD=%~4
 set buildCMD=%~5
 
 if "%sam_path%"=="help" (
-	echo sam_run ^<sam_path^> ^<source_code_path^> ^<language^> ^<clean_cmd^> ^<build_cmd^>
-	echo #Eg: sam_run "D:\Programs\SAM_v7.3.0-windows\SAM_v7.3.0-windows\bin" "C:\SCS\CyberTool\CyberTool" cs "dotnet clean CyberTool.sln -c Release" "dotnet build cyber_core\cyber_core.csproj -c Release --no-restore"
+	echo %scriptName% ^<sam_path^> ^<source_code_path^> ^<language^> ^<clean_cmd^> ^<build_cmd^>
+	echo #Eg: %scriptName% "D:\Programs\SAM_v7.3.0-windows\SAM_v7.3.0-windows\bin" "C:\SCS\CyberTool\CyberTool" cs "dotnet clean CyberTool.sln -c Release" "dotnet build cyber_core\cyber_core.csproj -c Release --no-restore"
 	exit /b 0
 )
 
 if "%sam_path%"=="--help" (
-	echo sam_run ^<sam_path^> ^<source_code_path^> ^<language^> ^<clean_cmd^> ^<build_cmd^>
-	echo #Eg: sam_run "D:\Programs\SAM_v7.3.0-windows\SAM_v7.3.0-windows\bin" "C:\SCS\CyberTool\CyberTool" cs "dotnet clean CyberTool.sln -c Release" "dotnet build cyber_core\cyber_core.csproj -c Release --no-restore"
+	echo %scriptName% ^<sam_path^> ^<source_code_path^> ^<language^> ^<clean_cmd^> ^<build_cmd^>
+	echo #Eg: %scriptName% "D:\Programs\SAM_v7.3.0-windows\SAM_v7.3.0-windows\bin" "C:\SCS\CyberTool\CyberTool" cs "dotnet clean CyberTool.sln -c Release" "dotnet build cyber_core\cyber_core.csproj -c Release --no-restore"
 	exit /b 0
 )
 
 if "%sam_path%"=="-h" (
-	echo sam_run ^<sam_path^> ^<source_code_path^> ^<language^> ^<clean_cmd^> ^<build_cmd^>
-	echo #Eg: sam_run "D:\Programs\SAM_v7.3.0-windows\SAM_v7.3.0-windows\bin" "C:\SCS\CyberTool\CyberTool" cs "dotnet clean CyberTool.sln -c Release" "dotnet build cyber_core\cyber_core.csproj -c Release --no-restore"
+	echo %scriptName% ^<sam_path^> ^<source_code_path^> ^<language^> ^<clean_cmd^> ^<build_cmd^>
+	echo #Eg: %scriptName% "D:\Programs\SAM_v7.3.0-windows\SAM_v7.3.0-windows\bin" "C:\SCS\CyberTool\CyberTool" cs "dotnet clean CyberTool.sln -c Release" "dotnet build cyber_core\cyber_core.csproj -c Release --no-restore"
+	exit /b 0
+)
+
+if "%sam_path%"=="-help" (
+	echo %scriptName% ^<sam_path^> ^<source_code_path^> ^<language^> ^<clean_cmd^> ^<build_cmd^>
+	echo #Eg: %scriptName% "D:\Programs\SAM_v7.3.0-windows\SAM_v7.3.0-windows\bin" "C:\SCS\CyberTool\CyberTool" cs "dotnet clean CyberTool.sln -c Release" "dotnet build cyber_core\cyber_core.csproj -c Release --no-restore"
+	exit /b 0
+)
+
+if "%sam_path%"=="" (
+	setlocal EnableDelayedExpansion
+	for /F %%a in ('echo prompt $E ^| cmd') do (
+		set "ESC=%%a"
+	)
+	echo. 
+	echo. 
+	echo !ESC![33m========WELCOME TO SAM SCRIPT=========!ESC![0m
+	echo To run the script please use:
+	echo    !ESC![1m%scriptName%!ESC![0m !ESC![31m^<sam_path^>!ESC![0m ^
+!ESC![37m^<source_code_path^>!ESC![0m ^
+!ESC![32m^<language^>!ESC![0m ^
+!ESC![35m^<clean_cmd^>!ESC![0m ^
+!ESC![36m^<build_cmd^>!ESC![0m
+
+	echo    #Eg: !ESC![1m%scriptName%!ESC![0m !ESC![31m"D:\Programs\SAM_v7.3.0-windows\SAM_v7.3.0-windows\bin"!ESC![0m ^
+!ESC![37m"C:\SCS\CyberTool\CyberTool"!ESC![0m ^
+!ESC![32mcs!ESC![0m ^
+!ESC![35m"dotnet clean CyberTool.sln -c Release"!ESC![0m ^
+!ESC![36m"dotnet build cyber_core\cyber_core.csproj -c Release --no-restore"!ESC![0m
+
+	echo. 
+	
+	echo  #!ESC![1mAnd please make sure to download the SAM tool at:!ESC![0m !ESC![34;4mhttps://bart.sec.samsung.net/artifactory/sar-generic-local/SAM-Tools/SAM_v7.3.0_release/SAM_v7.3.0-windows.zip!ESC![0m.
+	echo  #After downloaded SAM tool for window version, extract the zip, and you will see the ./bin folder
+	echo.
+	echo  #!ESC![1mAbout SAM tool homepage:!ESC![0m !ESC![34;4mhttps://github.sec.samsung.net/RS7-Architectural-Refactoring/SAM-Tools!ESC![0m.
+	echo.
+	echo.
+	echo !ESC![33m======================================!ESC![0m
+	echo.
+	echo.
+	cmd
 	exit /b 0
 )
 
