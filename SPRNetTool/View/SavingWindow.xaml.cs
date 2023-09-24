b1b3f45dd5ca0d166f372bbfed128a5455841ceb
mariohieu09 <125845409+mariohieu09@users.noreply.github.com>
2023-09-24 07:01:38 -0700

    [#54] Feature mariohieu09 save current source p2

---
+++ SPRNetTool/View/SavingWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SPRNetTool.View
+{
+    /// <summary>
+    /// Interaction logic for SavingWindow.xaml
+    /// </summary>
+    public partial class SavingWindow : Window
+    {
+      
+
+       public SavingWindow(Window? owner = null)
+       {
+            InitializeComponent();
+            Owner = owner;
+       }
+
+
+       public void Show()
+       {
+            base.ShowDialog();
+            
+       }
+
+       public void CancelClick(object sender, RoutedEventArgs e)
+       {
+            Close();
+       } 
+        
+       public void AgreeClick(object sender, RoutedEventArgs e) 
+       {
+            
+       }
+
+    }
+}
+
