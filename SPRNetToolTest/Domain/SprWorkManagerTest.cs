b10fc63aa19bda03d7514d98244d8178959bf669
nqduy1 <trdchannel199612@gmail.com>
2023-11-18 19:27:20 +0700

    [#82] Implement SprWorkManagerTest

--- SPRNetToolTest/Domain/SprWorkManagerTest.cs
+++ SPRNetToolTest/Domain/SprWorkManagerTest.cs
@@ -1,20 +1,45 @@
-﻿using SPRNetTool.Domain;
+﻿using SPRNetTool.Data;
+using SPRNetTool.Domain;
 using SPRNetTool.Domain.Base;
 using SPRNetTool.Domain.Utils;
 using SPRNetTool.Utils;
+using SPRNetToolTest.Utils;
 
 namespace SPRNetToolTest.Domain
 {
     internal class SprWorkManagerTest
     {
-        private string sprFilePath = "Resources\\test.spr";
-        private string pngFilePath = "Resources\\test.png";
+        private class SprWorkManagerTestObject : SprWorkManager
+        {
+            public long GetFrameDataBegPosCache()
+            {
+                return FrameDataBegPos;
+            }
+
+            public FrameRGBA[]? GetFrameDataCache()
+            {
+                return FrameData;
+            }
+        }
+
+
+        private string _sprFilePath = "Resources\\test.spr";
+        private string _binFilePath = "Resources\\test.bin";
+        private string _pngFilePath = "Resources\\test.png";
+        private string _12345sprFilePath = "Resources\\12345.spr";
+        private string _1binFilePath = "Resources\\1.bin";
+        private string _2binFilePath = "Resources\\2.bin";
+        private string _3binFilePath = "Resources\\3.bin";
+        private string _4binFilePath = "Resources\\4.bin";
+        private string _5binFilePath = "Resources\\5.bin";
         private ISprWorkManager sprWorkManager;
+        private SprWorkManagerTestObject sprWorkManagerTestObject;
 
         [SetUp]
         public void Setup()
         {
-            sprWorkManager = new SprWorkManager();
+            sprWorkManager = new SprWorkManagerTestObject();
+            sprWorkManagerTestObject = (SprWorkManagerTestObject)sprWorkManager;
         }
 
         [TearDown]
@@ -26,16 +51,69 @@ namespace SPRNetToolTest.Domain
         [Test]
         public void test_SaveBitmapSourceToSprFile()
         {
-            string imagePath = pngFilePath.FullPath();
+            string imagePath = _pngFilePath.FullPath();
             var bmpSource = sprWorkManager.LoadBitmapFromFile(imagePath);
             Assert.NotNull(bmpSource);
             sprWorkManager.SaveBitmapSourceToSprFile(bmpSource, "Resources\\test_SaveBitmapSourceToSprOutput.spr");
+
+            using (FileStream fs = new FileStream("Resources\\test_SaveBitmapSourceToSprOutput.spr", FileMode.Open, FileAccess.Read))
+            {
+                var initResult = sprWorkManager.InitWorkManagerFromSprFile(fs);
+                Assert.That(initResult);
+                Assert.That(sprWorkManager.FileHead.GlobalHeight * sprWorkManager.FileHead.GlobalWidth == 90000);
+
+                var frameData1Byte = TestUtil.ConvertPaletteColorArrayToByteArray(
+                   sprWorkManagerTestObject.GetFrameDataCache()![0].originDecodedFrameData);
+                var frameData1FromFile = TestUtil.ReadBytesFromFile(_binFilePath);
+                Assert.That(TestUtil.AreByteArraysEqual(frameData1Byte, frameData1FromFile!));
+            }
+        }
+
+        [Test]
+        public void test_InitWorkManagerFromSprFile_file12345()
+        {
+            using (FileStream fs = new FileStream(_12345sprFilePath, FileMode.Open, FileAccess.Read))
+            {
+                var initResult = sprWorkManager.InitWorkManagerFromSprFile(fs);
+                Assert.That(initResult);
+                Assert.That(sprWorkManager.FileHead.GlobalHeight * sprWorkManager.FileHead.GlobalWidth == 90000);
+
+
+                var frameData1Byte = TestUtil.ConvertPaletteColorArrayToByteArray(
+                    sprWorkManagerTestObject.GetFrameDataCache()![0].originDecodedFrameData);
+                var frameData1FromFile = TestUtil.ReadBytesFromFile(_1binFilePath);
+                Assert.That(TestUtil.AreByteArraysEqual(frameData1Byte, frameData1FromFile!));
+
+
+                var frameData2Byte = TestUtil.ConvertPaletteColorArrayToByteArray(
+                    sprWorkManagerTestObject.GetFrameDataCache()![1].originDecodedFrameData);
+                var frameData2FromFile = TestUtil.ReadBytesFromFile(_2binFilePath);
+                Assert.That(TestUtil.AreByteArraysEqual(frameData2Byte, frameData2FromFile!));
+
+
+                var frameData3Byte = TestUtil.ConvertPaletteColorArrayToByteArray(
+                    sprWorkManagerTestObject.GetFrameDataCache()![2].originDecodedFrameData);
+                var frameData3FromFile = TestUtil.ReadBytesFromFile(_3binFilePath);
+                Assert.That(TestUtil.AreByteArraysEqual(frameData3Byte, frameData3FromFile!));
+
+
+                var frameData4Byte = TestUtil.ConvertPaletteColorArrayToByteArray(
+                    sprWorkManagerTestObject.GetFrameDataCache()![3].originDecodedFrameData);
+                var frameData4FromFile = TestUtil.ReadBytesFromFile(_4binFilePath);
+                Assert.That(TestUtil.AreByteArraysEqual(frameData4Byte, frameData4FromFile!));
+
+
+                var frameData5Byte = TestUtil.ConvertPaletteColorArrayToByteArray(
+                    sprWorkManagerTestObject.GetFrameDataCache()![4].originDecodedFrameData);
+                var frameData5FromFile = TestUtil.ReadBytesFromFile(_5binFilePath);
+                Assert.That(TestUtil.AreByteArraysEqual(frameData5Byte, frameData5FromFile!));
+            }
         }
 
         [Test]
         public void test_InitWorkManagerFromSprFile()
         {
-            using (FileStream fs = new FileStream(sprFilePath, FileMode.Open, FileAccess.Read))
+            using (FileStream fs = new FileStream(_sprFilePath, FileMode.Open, FileAccess.Read))
             {
                 var initResult = sprWorkManager.InitWorkManagerFromSprFile(fs);
                 Assert.That(initResult);
