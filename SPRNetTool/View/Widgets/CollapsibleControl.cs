368b04f79b7f459e2c06c37bf106ae6a8930f9fd
nqduy1 <trdchannel199612@gmail.com>
2023-11-29 10:02:53 +0700

    [#173] Tính năng collapse các vùng điều chỉnh spr

---
+++ SPRNetTool/View/Widgets/CollapsibleControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using SPRNetTool.Utils;
+
+namespace SPRNetTool.View.Widgets
+{
+    public class CollapsibleControl : UserControl
+    {
+        public static readonly DependencyProperty HeaderProperty =
+           DependencyProperty.Register(
+               "Header",
+               typeof(string),
+               typeof(CollapsibleControl),
+               new PropertyMetadata(default(string), propertyChangedCallback: OnHeaderChanged));
+
+        private static void OnHeaderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            d.IfIs<CollapsibleControl>(it =>
+            {
+                it.headerTextBlock?.Apply(it => it.Text = e.NewValue.ToString());
+            });
+        }
+
+        public string Header
+        {
+            get { return GetValue(HeaderProperty)?.ToString() ?? ""; }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        private Button? collapseButton;
+        private TextBlock? headerTextBlock;
+        private bool isCollapse = false;
+
+        public CollapsibleControl()
+        {
+        }
+
+        public override void OnApplyTemplate()
+        {
+            collapseButton = GetTemplateChild("CollapseButton") as Button;
+            headerTextBlock = GetTemplateChild("Header") as TextBlock;
+            headerTextBlock?.Apply(it => it.Text = Header);
+            if (collapseButton != null)
+            {
+                collapseButton.Click += CollapseButton_Click;
+            }
+        }
+
+        private void CollapseButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            isCollapse = !isCollapse;
+            Content.IfIs<UIElement>(it =>
+            {
+                it.Visibility = isCollapse ? Visibility.Collapsed : Visibility.Visible;
+            });
+        }
+    }
+}
