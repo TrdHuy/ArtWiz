54bd0bbaffe7a615e6c3e9f94c0d56d49e7a599e
Quoc Cuong <cuongquoc000000@gmail.com>
2025-02-15 12:51:22 +0700

    [#346] Correct CodeBlock to GUI

---
+++ SPRNetTool/Utils/DoubleUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtWiz.Utils
+{
+    internal class DoubleUtil
+    {
+        internal const double DBL_EPSILON = 2.2204460492503131e-016; /* smallest such that 1.0+DBL_EPSILON != 1.0 */
+
+        public static bool IsZero(double value)
+        {
+            return Math.Abs(value) < 10.0 * DBL_EPSILON;
+        }
+    }
+}
