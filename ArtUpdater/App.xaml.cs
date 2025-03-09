04371ef2d66c404cdbbb2507a9cb8fc70552b0ad
Quoc Cuong <cuongquoc000000@gmail.com>
2025-03-09 18:33:38 +0700

    Thêm logic kiểm tra version info, chỉ extract những file cần thiết

--- ArtUpdater/App.xaml.cs
+++ ArtUpdater/App.xaml.cs
@@ -36,12 +36,12 @@ namespace ArtUpdater
                     mLogger.I($"ArtUpdater started with ZIP File: {ZipFilePath}, " +
                         $"Install Path: {InstallPath}");
                 }
-               
+
             }
             // FOR TEST ONLY 
-            ZipFilePath = "C:\\Users\\Hp\\Desktop\\temp\\test\\bin\\Rampart\\ArtWiz\\SPRNetToolTest\\Resources\\ArtWiz.zip";
-            InstallPath = "C:\\Users\\Hp\\Desktop\\temp\\test\\bin\\Rampart\\ArtWiz\\SPRNetTool\\bin\\x64\\Debug\\ForTestUpdater";
-            PrioritySearchingProcess = new List<string> { "ArtWiz" };
+            //    ZipFilePath = "C:\\Users\\Hp\\Desktop\\temp\\test\\bin\\Rampart\\ArtWiz\\SPRNetToolTest\\Resources\\ArtWiz.zip";
+            //    InstallPath = "C:\\Users\\Hp\\Desktop\\temp\\test\\bin\\Rampart\\ArtWiz\\SPRNetTool\\bin\\x64\\Debug\\ForTestUpdater";
+            //    PrioritySearchingProcess = new List<string> { "ArtWiz" };
         }
     }
 
